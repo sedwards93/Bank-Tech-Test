1d31a4c5f22acb0f47899daa91cd6d9c34fff1c4
Symion Edwards <symion.edwards@gmail.com>
2021-01-11 12:03:08 +0000

    New instances of the Account class are initialized with a balance of 0

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,6 @@
 class Account
-
+  attr_reader :balance
+  def initialize
+    @balance = 0
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -5,5 +5,8 @@ describe Account do
     it 'account is an instance of the Account class' do
       expect(subject).to be_an_instance_of(Account)
     end
+    it 'instances of the Account class are initialized with a balance of 0' do
+      expect(subject.balance).to eq(0)
+    end
   end
 end
