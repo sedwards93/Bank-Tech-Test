c149ee1be74c234c08116a963fe71e42a67f1b68
Symion Edwards <symion.edwards@gmail.com>
2021-01-11 15:19:00 +0000

    Written another test for transaction calss

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,2 +1,9 @@
 class Transaction
+  attr_reader :date, :credit, :debit, :balance
+  def initialize(date: Time.new, credit: nil , debit: nil, balance: nil)
+    @date = date.strftime('%d/%m/%Y')
+    @credit = credit
+    @debit = debit
+    @balance = balance
+  end
 end
--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,11 +1,19 @@
 require 'transaction'
 
 describe Transaction do
-
+  let(:transaction) {Transaction.new(date: "30/12/1993", credit: nil, debit: nil, balance: 0 )}
   describe '#Initialize' do
     it 'transaction is an instance of the Transaction class' do
       expect(subject).to be_an_instance_of(Transaction)
     end
+    it 'Instances of transaction class have a date instance variable with default equivalent to today\'s date' do
+      allow(Time).to receive(:new).and_return Time.new(2011, 11, 11)
+      expect(subject.date).to eq("11/11/2011")
+    end
+    it 'Instances of transaction class have a credit instance variable with default value of nil' do
+      expect(subject.credit).to eq(nil)
+    end
+    
   end
   
 end
